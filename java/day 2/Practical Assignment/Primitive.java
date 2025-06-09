1ba2619861e3c39a65bead1830f2171f886569e8
Ompatil2007 <om7341018@gmail.com>
2025-06-09 22:30:46 +0530

    Day 1

---
+++ java/day 2/Practical Assignment/Primitive.java
@@ -0,0 +1,29 @@
+package Demo.java;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		byte byteVar = 127;
+        short shortVar = 32000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+ 
+        float floatVar = 10.5f;
+        double doubleVar = 20.99;
+
+        char charVar = 'A';
+
+        boolean booleanVar = true;
+
+        System.out.println("byte value: " + byteVar);
+        System.out.println("short value: " + shortVar);
+        System.out.println("int value: " + intVar);
+        System.out.println("long value: " + longVar);
+        System.out.println("float value: " + floatVar);
+        System.out.println("double value: " + doubleVar);
+        System.out.println("char value: " + charVar);
+        System.out.println("boolean value: " + booleanVar);
+
+	}
+
+}
