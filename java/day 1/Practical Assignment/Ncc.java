b85070bbda498dcbdaff4a88f229b28ef66104d2
Ompatil2007 <om7341018@gmail.com>
2025-06-09 22:28:52 +0530

    Day 1

---
+++ java/day 1/Practical Assignment/Ncc.java
@@ -0,0 +1,15 @@
+package Demo.java;
+
+public class Ncc {
+
+	public static void main(String[] args) {
+		String Name="Om";
+		String course="Full Stack Using Java";
+		String city="Nashik";
+		System.out.println("Name:"+Name);
+		System.out.println("Name:"+course);
+		System.out.println("Name:"+city);
+
+	}
+
+}
