037e7fd63bbd7e00f846bf95537414b301379ed5
Ompatil2007 <om7341018@gmail.com>
2025-06-12 16:10:48 +0530

    Day 8

---
+++ java/day 9/Practical Assignment/SentenceSplit.java
@@ -0,0 +1,17 @@
+package Demo.java;
+
+public class SentenceSplit {
+
+	public static void main(String[] args) {
+	String s="Hello Thailand";
+	
+	String[] words=s.split("");
+	for(String word:words) {
+		System.out.println(word
+				);
+	}
+	
+
+	}
+
+}
